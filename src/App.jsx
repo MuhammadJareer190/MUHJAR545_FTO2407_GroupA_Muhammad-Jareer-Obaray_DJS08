aae25463b5701ffaf8c19dedaff395362c3c1311
Muhammad Jareer <jareerobaray@icloud.com>
2024-11-17 08:50:13 +0200

    Added two components

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./Home.jsx"
 import About from "./About.jsx"
 import './server.jsx'
+import Vans from './Vans.jsx';
+import VanDetail from './VanDetail.jsx';
 
 function App() {
   return (
@@ -27,11 +29,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
