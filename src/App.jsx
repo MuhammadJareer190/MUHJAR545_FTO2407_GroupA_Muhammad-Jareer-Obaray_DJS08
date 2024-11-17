955a656db434faabb22e61c346bc64cf39b3a78d
Muhammad Jareer <jareerobaray@icloud.com>
2024-11-17 10:09:36 +0200

    Changed files layout

--- src/App.jsx
+++ src/App.jsx
@@ -16,27 +16,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./Home.jsx"
-import About from "./About.jsx"
+import Home from "../pages/Home.jsx"
+import About from "../pages/About.jsx"
+import Vans from '../pages/Vans/Vans.jsx';
+import VanDetail from '../pages/Vans/VanDetail.jsx';
+import Dashboard from '../pages/Host/Dashboard.jsx';
+import Income from '../pages/Host/Income.jsx';
+import Reviews from '../pages/Host/Reviews.jsx';
+import Layout from '../components/Layout.jsx';
+
 import './server.jsx'
-import Vans from './Vans.jsx';
-import VanDetail from './VanDetail.jsx';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
